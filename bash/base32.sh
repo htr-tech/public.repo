5e983a34cd1920a36f1beb266fb8d824ea299358
htr-tech <tahmidrayat@gmail.com>
2022-08-24 12:32:03 +0600

    Format

--- bash/base32.sh
+++ bash/base32.sh
@@ -3,17 +3,17 @@
 # Encode Bash Script with Base32
 
 obfuscate() {
-printf "#!/bin/bash
+	printf "#!/bin/bash
 mew=\$(mktemp)
 base32 -d  > \${mew} << hulu
 $(base32 $1)
 hulu
 source \${mew}
-rm -rf \${mew}" > $2
+rm -rf \${mew}" >$2
 }
 
 if [[ -e $1 && ! $2 == "" ]]; then
-    obfuscate $1 $2
+	obfuscate $1 $2
 else
-    echo -e "Usage: "$0" <input> <output>"
+	echo -e "Usage: "$0" <input> <output>"
 fi
--- bash/crypter.sh
+++ bash/crypter.sh
@@ -5,71 +5,76 @@
 ##################################
 
 install_ssl() {
-    [ ! $(command -v openssl) ] && {
-        if [ $(command -v pkg) ]; then
-            pkg install openssl-tool -y
-        elif [ $(command -v apt) ]; then
-            sudo apt install openssl -y
-        else
-            echo "[!] Unfamiliar Package Manager" && exit 1
-        fi
-    }
+	[ ! $(command -v openssl) ] && {
+		if [ $(command -v pkg) ]; then
+			pkg install openssl-tool -y
+		elif [ $(command -v apt) ]; then
+			sudo apt install openssl -y
+		else
+			echo "[!] Unfamiliar Package Manager" && exit 1
+		fi
+	}
 }
 
-install_ssl ; clear
+install_ssl
+clear
 
 encode() {
-    input="$1"
-    output="$input.enc"
-    openssl enc -aes-256-ecb -a -pbkdf2 -e -in "$input" -out "$output" -k "$2"
-    [ -e "$output" ] && echo "[-] Output : " $output ; echo "[-] Passwd : " $3
+	input="$1"
+	output="$input.enc"
+	openssl enc -aes-256-ecb -a -pbkdf2 -e -in "$input" -out "$output" -k "$2"
+	[ -e "$output" ] && echo "[-] Output : " $output
+	echo "[-] Passwd : " $3
 }
 
 decode() {
-    input="$1"
-    output="${input%.*}"
-    openssl enc -aes-256-ecb -a -pbkdf2 -d -in "$input" -out "$output" -k "$2" 2>/dev/null
-    [ $? != 0 ] && echo "[!] Wrong Password" && sleep 1 && rm -f "$output" && exit 1
-    [ -e "$output" ] && echo -e "\n[-] Output : " $output
+	input="$1"
+	output="${input%.*}"
+	openssl enc -aes-256-ecb -a -pbkdf2 -d -in "$input" -out "$output" -k "$2" 2>/dev/null
+	[ $? != 0 ] && echo "[!] Wrong Password" && sleep 1 && rm -f "$output" && exit 1
+	[ -e "$output" ] && echo -e "\n[-] Output : " $output
 }
 
 check_file() {
-    file="$1$2"
-    [ ! -e "$file" ] && {
-        read -p "[-] Filename: " nname
-        file="${nname}$2"
-    }
-    [ ! -e "$file" ] && echo "[!] No such files" && sleep 1 && exit 1
-    filename="$file"
+	file="$1$2"
+	[ ! -e "$file" ] && {
+		read -p "[-] Filename: " nname
+		file="${nname}$2"
+	}
+	[ ! -e "$file" ] && echo "[!] No such files" && sleep 1 && exit 1
+	filename="$file"
 }
 
 passwrd() {
-    [ "$1" != '' ] && {
-        hash_pass=$(echo -n $1 | base64 | sha256sum | awk '{print $1}')
-        } || {
-        echo "[!] Password can't be empty" && exit 1
-    }
+	[ "$1" != '' ] && {
+		hash_pass=$(echo -n $1 | base64 | sha256sum | awk '{print $1}')
+	} || {
+		echo "[!] Password can't be empty" && exit 1
+	}
 }
 
 main_menu() {
 	read -n1 -p "[-] Select an Option [d|e] " enc_dec
-    echo
-    case $enc_dec in
-        [dD] ) 
-            check_file "$1" ".7z.enc" # encoded extension
-            read -sp "[-] Password: " typedpass
-            echo
-            passwrd "$typedpass"
-            decode "$filename" "$hash_pass" ;;
-        [eE] ) 
-            check_file "$1" ".7z" # base extension
-            read -p "[-] Password: " typedpass
-            echo
-            passwrd "$typedpass"
-            encode "$filename" "$hash_pass" "$typedpass" ;;
-        * ) 
-            echo "[!] Input d or e" && sleep 1 && exit 1 ;;
-    esac
+	echo
+	case $enc_dec in
+		[dD])
+			check_file "$1" ".7z.enc" # encoded extension
+			read -sp "[-] Password: " typedpass
+			echo
+			passwrd "$typedpass"
+			decode "$filename" "$hash_pass"
+			;;
+		[eE])
+			check_file "$1" ".7z" # base extension
+			read -p "[-] Password: " typedpass
+			echo
+			passwrd "$typedpass"
+			encode "$filename" "$hash_pass" "$typedpass"
+			;;
+		*)
+			echo "[!] Input d or e" && sleep 1 && exit 1
+			;;
+	esac
 }
 
 main_menu "data" # File Name (data.7z)
