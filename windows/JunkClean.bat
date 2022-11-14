2d6c26b22e93aa8f37f3735886e919d8c2960b9f
htr-tech <tahmidrayat@gmail.com>
2022-11-14 18:06:40 +0600

    Clean system tmp files

---
+++ windows/JunkClean.bat
@@ -0,0 +1,25 @@
+@echo off
+if not “%1” == “max” start /MIN cmd /c %0 max & exit/b
+echo.
+echo Cleaning system junk files, please wait . . .
+echo.
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+rd /s /q %windir%\temp & md %windir%\temp
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q “%userprofile%\Local Settings\Temporary Internet Files\*.*”
+del /f /s /q “%userprofile%\Local Settings\Temp\*.*”
+del /f /s /q “%userprofile%\recent\*.*”
+ipconfig /flushdns
+echo.
+echo Junk files clean is finished!
+echo. & pause
+
